037742ccd3078dbae37768ed2aca593a7df0ec6e
ClaudioLoGiudice7 <claudiologiudice6@gmail.com>
2023-03-31 22:59:14 +0200

    entering milestone 1

---
+++ database/migrations/2023_03_31_183112_create_songs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("album");
+            $table->string("author");
+            $table->string("editor");
+            $table->integer("length");
+            $table->string("poster");
+            $table->string("release_date");
+            $table->boolean("cover");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+};
